a8cb654113cb77dca9c9896da988471c0477358d
Vinicius Vieira <viniciusmv91@gmail.com>
2026-04-18 23:46:36 -0300

    Add RemoverFundoCommandValidator for command validation
    Introduced RemoverFundoCommandValidator using FluentValidation to ensure the Codigo property is required and does not exceed Fundo.CodigoMaxLength. Custom error messages are included for validation failures.

---
+++ src/CaseItau.Application/Fundos/RemoverFundo/RemoverFundoCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using CaseItau.Domain.Fundos;
+using FluentValidation;
+
+namespace CaseItau.Application.Fundos.RemoverFundo;
+
+public class RemoverFundoCommandValidator : AbstractValidator<RemoverFundoCommand>
+{
+    public RemoverFundoCommandValidator()
+    {
+        RuleFor(x => x.Codigo)
+            .NotEmpty()
+            .WithMessage("Código é obrigatório.")
+            .MaximumLength(Fundo.CodigoMaxLength)
+            .WithMessage($"Código não pode ter mais de {Fundo.CodigoMaxLength} caracteres.");
+    }
+}
