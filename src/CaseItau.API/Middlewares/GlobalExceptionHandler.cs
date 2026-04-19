020578ce0cf4b4b09b9747a11d39af6f95cae0df
Vinicius Vieira <viniciusmv91@gmail.com>
2026-04-18 21:32:29 -0300

    Simplify global exception handler to generic 500 response
    Removed custom exception mapping and detailed error responses from the global exception handler. Now, all unhandled exceptions return a generic 500 Server Error ProblemDetails with only the traceId included. Also removed FluentValidation-specific handling and related code.

--- src/CaseItau.API/Middlewares/GlobalExceptionHandler.cs
+++ src/CaseItau.API/Middlewares/GlobalExceptionHandler.cs
@@ -1,4 +1,4 @@
-﻿using FluentValidation;
+﻿using CaseItau.Application.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,64 +24,19 @@ public sealed class GlobalExceptionHandler : IExceptionHandler
             httpContext.Request.Path,
             httpContext.Request.Method);
 
-        var exceptionDetails = GetExceptionDetails(exception);
-
         var problemDetails = new ProblemDetails
         {
-            Status = exceptionDetails.Status,
-            Type = exceptionDetails.Type,
-            Title = exceptionDetails.Title,
-            Detail = exceptionDetails.Detail,
-            Instance = httpContext.Request.Path
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Server Error",
+            Instance = httpContext.Request.Path,
         };
+
         problemDetails.Extensions["traceId"] = httpContext.TraceIdentifier;
 
-        if (_env.IsDevelopment())
-        {
-            problemDetails.Detail = exception.Message;
-            problemDetails.Extensions["stackTrace"] = exception.StackTrace;
-
-            if (exception.InnerException is not null)
-            {
-                problemDetails.Extensions["innerException"] = new
-                {
-                    message = exception.InnerException.Message,
-                    stackTrace = exception.InnerException.StackTrace
-                };
-            }
-        }
-
-        httpContext.Response.StatusCode = exceptionDetails.Status;
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
 
         await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
 
         return true;
     }
-
-    private static ExceptionDetails GetExceptionDetails(Exception exception)
-    {
-        return exception switch
-        {
-            ValidationException validationException => new ExceptionDetails(
-                StatusCodes.Status400BadRequest,
-                "ValidationFailure",
-                "Validation error",
-                "One or more validation errors has occurred",
-                validationException.Errors),
-
-            _ => new ExceptionDetails(
-                StatusCodes.Status500InternalServerError,
-                "ServerError",
-                "Server error",
-                "An unexpected error has occurred",
-                null)
-        };
-    }
-
-    internal sealed record ExceptionDetails(
-        int Status,
-        string Type,
-        string Title,
-        string Detail,
-        IEnumerable<object>? Errors);
 }
