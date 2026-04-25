d4357abb1b2d0ec0ab4ddd62d9fcf8bfc5e043e6
Vinicius Vieira <viniciusmv91@gmail.com>
2026-04-24 21:35:29 -0300

    Add functional test for Listar Fundos API endpoint
    Introduced ListarFundosTests to verify the /api/Fundo endpoint.
    The test ensures a GET request returns HTTP 200 OK and a
    non-empty list of funds, validating key fields for each fund.

---
+++ test/CaseItau.Api.FunctionalTests/Fundos/ListarFundos/ListarFundosTests.cs
@@ -0,0 +1,36 @@
+﻿using CaseItau.Api.FunctionalTests.Common;
+using CaseItau.Application.Fundos;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace CaseItau.Api.FunctionalTests.Fundos.ListarFundos;
+
+public class ListarFundosTests : BaseFunctionalTest
+{
+    private const string Endpoint = "/api/Fundo";
+
+    public ListarFundosTests(FunctionalTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task ListarFundos_Deve_RetornarOk_Com_ListaDeFundos()
+    {
+        // Act
+        var response = await HttpClient.GetAsync(Endpoint);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var fundos = await response.Content.ReadFromJsonAsync<List<FundoResponse>>();
+        fundos.Should().NotBeNullOrEmpty();
+        fundos!.Should().AllSatisfy(fundo =>
+        {
+            fundo.Codigo.Should().NotBeNullOrWhiteSpace();
+            fundo.Nome.Should().NotBeNullOrWhiteSpace();
+            fundo.Cnpj.Should().NotBeNullOrWhiteSpace();
+            fundo.NomeTipo.Should().NotBeNullOrWhiteSpace();
+        });
+    }
+}
